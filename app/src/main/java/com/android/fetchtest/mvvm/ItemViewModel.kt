aac96ebd6de73d00ee59e066bd65faf6d7304568
NikithSai <nikith.kuruvella@smscountry.com>
2024-09-08 22:24:22 +0530

    Minor Change.

--- app/src/main/java/com/android/fetchtest/mvvm/ItemViewModel.kt
+++ app/src/main/java/com/android/fetchtest/mvvm/ItemViewModel.kt
@@ -5,18 +5,12 @@ import androidx.lifecycle.viewModelScope
 import com.android.fetchtest.common.UIState
 import com.android.fetchtest.usecase.ApiUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
